5712a74f7c0b13dc46cbac39eaa88b5cbb891cbc
kunalm-webelight <kunalm@webelight.co.in>
2023-06-02 14:54:01 +0530

    first component added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,16 @@
-function Home(){
-    return null;
-}
-function test(){
-    return "this function is in test branch";
+export default function Home() {
+  return (
+    <>
+      <div class="top-block">
+        <div class="leftcontent">
+          <div>info@mmt.com</div>
+          <div>1+(111)1111 1111</div>
+        </div>
+        <div class="rightcontent">
+          <div>Twitter</div>
+          <div>Facebook</div>
+        </div>
+      </div>
+    </>
+  );
 }
