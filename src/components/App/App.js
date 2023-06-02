3e344c066d2024fc6e2e42495ba6a195603c9aa7
kunalm-webelight <kunalm@webelight.co.in>
2023-06-02 16:50:47 +0530

    All components added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,53 @@
-import './App.css';
-import Home from '../Home/Home.js'
+import "./App.css";
+import Home from "../Home/Home.js";
+import Contact from "../Contact/Contact";
+import Blog from "../Blog/Blog";
+import Services from "../Services/Services";
+import { Route, Routes, Link, BrowserRouter } from "react-router-dom";
 function App() {
   return (
-    <Home/>
-    
+    //top block
+    <>
+      <div class="top-block">
+        <div class="leftcontent">
+          <div>info@mmt.com</div>
+          <div>1+(111)1111 1111</div>
+        </div>
+        <div class="rightcontent">
+          <div>Twitter</div>
+          <div>Facebook</div>
+        </div>
+      </div>
+
+      <div class="header-block">
+        <div class="brand-image">
+          <img alt="MakeMyTrip" class="image" />
+        </div>
+      <BrowserRouter>
+        <nav class="nav-bar">
+          <Link to="/home">Home</Link>
+          <Link to="/services">Services</Link>
+          <Link to="#NA">About Us</Link>
+          <Link to="#NA">Why Us</Link>
+          <Link to="#NA">Testimonials</Link>
+          <Link to="/blog">Blog</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
+        
+        <Routes>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+          <Route path="/blog" element={<Blog />}></Route>
+          <Route path="/services" element={<Services />}></Route>
+        </Routes>
+      </BrowserRouter>
+      </div>
+
+    </>
+
+    //home compo with form
+    //blog compo with blog txt
+    //contact compo with contact txt
   );
 }
 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,16 +1,76 @@
 export default function Home() {
   return (
     <>
-      <div class="top-block">
-        <div class="leftcontent">
-          <div>info@mmt.com</div>
-          <div>1+(111)1111 1111</div>
+      <div class="formcontainer">
+        <h1>Some Form</h1>
+        <div class="formrows">
+          <fieldset>
+            <div class="formelements"><label for="email">Email:</label></div>
+            <div class="formelements">
+              <input type="text" id="email" name="email" placeholder="Email" />
+            </div>
+          </fieldset>
         </div>
-        <div class="rightcontent">
-          <div>Twitter</div>
-          <div>Facebook</div>
+
+        <hr />
+        <div class="formrows">
+          <fieldset>
+            <div class="formelements">
+              <label for="password">Password:</label>
+            </div>
+            <div class="formelements">
+              <input
+                type="text"
+                id="password"
+                name="password"
+                placeholder="Password"
+              />
+            </div>
+          </fieldset>
+        </div>
+        <div class="formrows">
+          <fieldset>
+            <div class="formelements">
+              <label for="confirm-password">Confirm-password</label>
+            </div>
+            <div class="formelements">
+              <input
+                type="text"
+                id="confirm-password"
+                name="confirm-password"
+                placeholder="Confirm-password"
+              />
+            </div>
+          </fieldset>
+        </div>
+        <div class="formrows">
+          <fieldset>
+            <div class="formelements">
+              <label for="secret-code"></label>Secret-code
+            </div>
+            <div class="formelements">
+              <input
+                type="text"
+                id="secret-code"
+                name="secret-code"
+                placeholder="Secret-code"
+                onchange="handleChange(event)"
+              />
+            </div>
+          </fieldset>
+        </div>
+        <div class="btnrow">
+          <div class="buttons"><input type="button" value="SignUp" /></div>
+          <div class="buttons">
+            <input
+              type="button"
+              id="signin"
+              value="SignIn"
+            />
+          </div>
         </div>
       </div>
+      
     </>
   );
 }
